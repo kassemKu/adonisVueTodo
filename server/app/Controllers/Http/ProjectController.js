328cd050a7d42bb4714515e9cc295fc6e07ba88b
KassemKu <arabisc95@gmail.com>
2018-08-04 23:05:05 +0300

    destroy with exception

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Project = use('App/Models/Project')
+const AuthorizationService = use ('App/Services/AuthorizationService')
 
 class ProjectController {
   async index({ auth })
@@ -18,6 +19,21 @@ class ProjectController {
       title
     })
     await user.projects().save(project)
+    return project
+  }
+
+  async destroy({ auth, params })
+  {
+    const user = await auth.getUser()
+    const { id } = params
+
+    const project = await Project.find(id);
+
+    // if(project.user_id !== user.id) {
+    //   return response.status(304)
+    // }
+    AuthorizationService.verifyPermission(project, user)
+    await project.delete()
     return project
   }
 }
