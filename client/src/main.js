19520a665a566eb8d404ebf352aacfe3f425a876
KassemKu <arabisc95@gmail.com>
2018-08-06 03:37:26 +0300

    start Frontend setup vue

---
+++ client/src/main.js
@@ -0,0 +1,18 @@
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index';
+
+sync(store, router);
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
---
+++ client/src/store/index.js
@@ -0,0 +1,23 @@
+import CreatePersistedState from 'vuex-persistedstate';
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: true,
+
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+
+  plugins: [
+    CreatePersistedState()
+  ],
+});
